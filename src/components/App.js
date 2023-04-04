300d35ba3ec54761061ecc5df3606f81b4d64940
AdityaTandulkar <adityatandulkar20@gmail.com>
2023-04-04 19:42:01 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,12 @@ import '../styles/App.css';
 
 const App = () => {
   //Write your code here
+  const query = new URLSearchParams(window.location.search);
+  return(
+    <div>
+      Employee {query.get("id") named {"name"} works as {query.designation}}
+    </div>
+  )
 }
 
 export default App;
